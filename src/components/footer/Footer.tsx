defbc531bc14eff9a0e4c8490c43c1fa1e34dd39
Taimello <tainarakarolina@gmail.com>
2023-07-28 10:11:47 -0300

    Atualização do icon e icone de reservado no copyrigth

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
     <>
         <div className="flex justify-center bg-lime-950 text-white">
           <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Organi | Copyright: </p>
+            <p className='text-xl font-bold'>Organi | &copy; Copyright: </p>
             <p className='text-lg'>Acesse nossas redes sociais</p>
             <div className='flex gap-2'>
               <LinkedinLogo size={48} weight='bold' />
