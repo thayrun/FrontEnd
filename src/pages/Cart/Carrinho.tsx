6544583d239a6696ec140916d2ce8324cac1d9ee
Sabrina <sabrinaprogram99@gmail.com>
2023-08-11 11:47:05 -0300

    Task 13 - Adição da lógica do carrinho e correções

--- src/pages/Cart/Carrinho.tsx
+++ src/pages/Cart/Carrinho.tsx
@@ -14,7 +14,6 @@ function Carrinho() {
                     <h1>{item.nome}</h1>
                     <h2>{item.descricao}</h2>
                     <h2>R${item.preco}</h2>
-                    {/* <h2>Quantidade{item.}</h2> */}
                     <div>
                         <button onClick={() => removerProduto(item.id)} className='text-white bg-red-500 hover:bg-red-400 items-center justify-center py-1 px-2'>Deletar do carrinho</button>
                     </div>
